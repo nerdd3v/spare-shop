7a0c1ceed19df0c51dbb885e884b429a34eea9da
n3rddev <n3rddev.code@gmail.com>
2025-10-28 12:44:49 +0530

    auth middleware created

--- backend/src/middlewares/authMW.ts
+++ backend/src/middlewares/authMW.ts
@@ -3,23 +3,30 @@ import jwt, { decode } from 'jsonwebtoken'
 
 const authMW = (req: Request, res: Response, next: NextFunction)=>{
 
-    //@ts-ignore
-    const authHeader = req.headers.authorization;
-
-    if(!authHeader){
-        return res.status(400).json({message: "auth header not found"})
+    try {
+        //@ts-ignore
+        const authHeader = req.headers.authorization;
+        
+        if(!authHeader){
+            return res.status(400).json({message: "auth header not found"})
+        }
+        
+        const token = authHeader.split(' ')[1];
+        
+        if(!token){
+            return res.status(400).json({message: "token not found"})
+        }
+        const decoded = jwt.verify(token, "cotton")
+        
+        if(!decoded){
+            return res.status(400).json({message:"invalid token cannot be decoded"})
+        }
+        //@ts-ignore
+        req.user = decoded.userId
+        
+        next()
+    } 
+    catch (error) {
+        return res.status(500).json({message: "internal server error"})
     }
-
-    const token = authHeader.split(' ')[1];
-
-    if(!token){
-        return res.status(400).json({message: "token not found"})
-    }
-    const decoded = jwt.verify(token, "cotton")
-
-    if(!decoded){
-        return res.status(400).json({message:"invalid token cannot be decoded"})
-    }
-
-    req.user = decoded.userId
 }
